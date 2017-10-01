b0aae3d1eaa24bd2ad979cf98500c2a76f2900b3
Dima-f <dafw@live.ru>
2017-10-02 00:24:52 +0300

    first commit

---
+++ models/database.js
@@ -0,0 +1,10 @@
+const { Client } = require('pg')
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo'
+const client = new Client(connectionString)
+
+client.connect()
+
+client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)', (err, res) => {
+  console.log(err ? err.stack : 'OK') // Hello World!
+  client.end()
+})
